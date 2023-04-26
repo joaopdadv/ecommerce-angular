fdf8908ff2d84456c7e4f2992fd8f6e9a7d736a7
joaopdadv <joaopdadv@gmail.com>
2023-04-26 16:30:51 -0300

    Primeiro commit

---
+++ 02-backend/spring-eccomerce/src/main/java/br/com/joaopedro/springeccomerce/SpringEccomerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaopedro.springeccomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEccomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEccomerceApplication.class, args);
+	}
+
+}
---
+++ 02-backend/spring-eccomerce/src/test/java/br/com/joaopedro/springeccomerce/SpringEccomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaopedro.springeccomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEccomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
